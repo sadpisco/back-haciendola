a6998a669dae5834b8f28c45c29a7f50db9dee93
Leonardo Risco <leo99153@gmail.com>
2024-05-10 23:34:02 -0500

    Auth function added to the backEnd

--- src/handlers/Users/AuthUser.js
+++ src/handlers/Users/AuthUser.js
@@ -1,10 +1,33 @@
 const { Users } = require('../../database.js');
 
-
 const AuthUser = async function(req, res){
-    const prueba = nanoid();
+    let attemps = 0;
     try {
-        res.status(202).send(prueba);
+        const { username, password } = req.body;
+        let foundUser = await Users.findAll({
+            where: {
+                username: username
+            }
+        });
+        if (foundUser.length > 0){
+            let matchingPass = await Users.findAll({
+                where: {
+                    username: username,
+                    password: password
+                }
+            });
+            if (matchingPass.length > 0){
+                res.status(202).send({
+                    status: true,
+                    authenticated: true
+                });
+            } else {
+                attemps++;
+                res.status(500).send(`Wrong password for ${username}.`);
+            };
+        } else {
+            res.status(500).send(`${username} is not registered on our platform.`);
+        };
     } catch (error) {
         console.log(error);
         res.status(500).send({ message: error.message });
--- src/handlers/Users/GetAllUsers.js
+++ src/handlers/Users/GetAllUsers.js
@@ -3,7 +3,18 @@ const { Users } = require('../../database.js');
 const GetAllUsers = async function(req, res){
     try {
         const allUsers = await Users.findAll();
-        res.status(202).json(allUsers);
+        if (allUsers.length == 0){
+            res.status(202).send({
+                status: true,
+                users: allUsers.length,
+                message:`There is zero registered users.`
+            });
+        } else {
+            res.status(202).json({
+                status: true,
+                users: allUsers.length,
+                allUsers});
+        };
     } catch (error) {
         console.log(error);
         res.status(500).send({ message: error.message });
