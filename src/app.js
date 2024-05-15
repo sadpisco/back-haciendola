00c7dfb100310337f01b8dec65291774eccc971f
Leonardo Risco <leo99153@gmail.com>
2024-05-15 16:33:03 -0500

    index + app

--- src/app.js
+++ src/app.js
@@ -2,16 +2,3 @@ require = require("esm")(module, {
     cjs: true
 });
 
-const express = require('express');
-const morgan = require ('morgan');
-const cors = require('cors');
-const router = require('./routes/router');
-
-
-const app = express();
-app.use(morgan('dev'));
-app.use(cors());
-app.use(express.json());
-app.use(router);
-
-module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 require('dotenv').config();
-const app = require('./app');
+
+const express = require('express');
+const morgan = require ('morgan');
+const cors = require('cors');
+const router = require('./routes/router');
+
+
+const app = express();
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+
+
+
 const { conn } = require('./database');
 const port = process.env.PORT || 3000;
 
@@ -10,3 +25,5 @@ conn.sync({force: false}).then(()=> {
 }).catch( error => console.log(error));
 
 console.log(`Server is listening on ${port}`);
+
+module.exports = app;
