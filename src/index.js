a0926108495d19e2190929091f941a9f27888804
Leonardo Risco <leo99153@gmail.com>
2024-05-15 15:20:34 -0500

    port

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ const { conn } = require('./database');
 const SERVER_PORT = process.env.SERVER_PORT;
 
 conn.sync({force: false}).then(()=> {
-    app.listen(SERVER_PORT, () => {
-        console.log(`Server listening on port ${SERVER_PORT}.`)
+    app.listen(process.env.PORT || SERVER_PORT, () => {
+        console.log(`Server listening on port ${process.env.PORT || SERVER_PORT}.`)
     });
 }).catch( error => console.log(error));
 
-console.log(`Server is listening on ${SERVER_PORT}`);
+console.log(`Server is listening on ${process.env.PORT || SERVER_PORT}`);
